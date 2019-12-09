1272268217f4f9542ad54fa4c421bebf9c3eb1ac
Katie Woolston <katiewoolston@geniesolutions.com.au>
2019-12-10 09:15:48 +1000

    add triangles exercise

---
+++ scripts/e1_triangles.ts
@@ -0,0 +1,37 @@
+/**
+ * Exercise 1
+ *
+ * Below is an array of triangles, defined by the length of each side.
+ *
+ * Update the script function to iterate through each one and determine
+ * what kind of triangle it is:
+ *   * scalene (all sides different)
+ *   * isoceles (two sides the same)
+ *   * equilateral (all sides the same)
+ * Print a represent
+ *
+ * Start by defining a 'Triangle' interface with each of the side attributes,
+ * then use this type when working with the triangles.
+ *
+ * Once you have finished, check for any linting issues by running 'npm run lint'.
+ *
+ *
+ */
+
+const triangles = [
+  {s1: 10, s2: 8, s3: 4},
+  {s1: 9, s2: 9, s3: 4},
+  {s1: 8, s2: 2, s3: 8},
+  {s1: 7, s2: 7, s3: 7},
+  {s1: 6, s2: 12, s3: 3},
+  {s1: 5, s2: 3, s3: 5},
+  {s1: 4, s2: 2, s3: 4},
+  {s1: 3, s2: 3, s3: 3},
+  {s1: 2, s2: 8, s3: 4},
+];
+
+export const script = (): void => {
+
+}
+script();
+
